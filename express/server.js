74a238199cc14a0ff52bdee0e5aabfbaf0998daa
Angel Hayling <angel.haylinguerra@hotmail.com>
2024-05-17 00:24:14 -0500

    Ping update

--- express/server.js
+++ express/server.js
@@ -12,7 +12,6 @@ router.get('/', (req, res) => {
   res.end();
 });
 router.get('/ping', (req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
   res.write('Pong!');
   res.end();
 });
